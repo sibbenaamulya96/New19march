bb85cee70f03570f8b5345bfd1868bfe60b98620
sibbenaamulya96 <sibbenaamulya96@gmail.com>
2021-03-19 20:51:12 +0530

    pushing data

---
+++ src/org/edu/Arts.java
@@ -0,0 +1,35 @@
+package org.edu;
+
+public class Arts extends Education {
+	public void bSc() {
+System.out.println("bsc");
+	}
+	public void bEd() {
+		System.out.println("bEd");
+
+	}
+	public void bA() {
+		System.out.println("bA");
+
+	}
+	public void bBA() {
+		System.out.println("bBA");
+
+	}
+	public void ug() {
+System.out.println("ug=22");
+	}
+	public void pg() {
+		System.out.println("pg=12");
+
+	}
+	public static void main(String[] args) {
+		Arts e=new Arts();
+		e.bA();
+		e.bBA();
+		e.bEd();
+		e.bSc();
+		e.pg();
+		e.ug();
+	}
+}
---
+++ src/org/edu/Education.java
@@ -0,0 +1,11 @@
+package org.edu;
+
+public class Education {
+	public void ug() {
+		System.out.println("ug=4");
+
+	}
+	public void pg() {
+		System.out.println("pg=7");
+	}
+}
